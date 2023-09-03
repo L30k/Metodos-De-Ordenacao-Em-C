322f0c730491f0b009ba5de1e71f6692c25bd95d
Leonardo Yago Nascimento Silva <71148287+L30k@users.noreply.github.com>
2023-09-03 17:34:14 -0300

    Update countingSort.c

--- countingSort.c
+++ countingSort.c
@@ -1,65 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void exibir(int vet[], int tam);
 int Maior(int vet[], int tam);
 void countingSort(int vet[], int tam);
 
-int main(){
+int main() {
     int tam = 10;
     int vet[tam];
-    
-    for(int i = 0; i < tam; i++){
-        vet[i] = rand()%100;
+
+    for (int i = 0; i < tam; i++) {
+        vet[i] = rand() % 100;
+        printf("|%d| ", vet[i]);
     }
-    
-    exibir(vet, tam);
-    
+    printf("\n");
+
     countingSort(vet, tam);
+
+    for (int i = 0; i < tam; i++) {
+        printf("|%d| ", vet[i]);
+    }
 
     return 0;
 }
 
-
-void exibir(int vet[], int tam){
-    printf("\n");
-    for(int i = 0; i < tam; i++){
-        printf("|%d| ", vet[i]);
-    }    
-}
-
-int Maior(int vet[], int tam){
+int Maior(int vet[], int tam) {
     int x = vet[0];
     for (int i = 1; i < tam; i++) {
-        if (vet[i] > x){
+        if (vet[i] > x) {
             x = vet[i];
         }
     }
     return x;
 }
 
-void countingSort(int vet[], int tam){
+void countingSort(int vet[], int tam) {
     int maior = Maior(vet, tam);
-    int vetAux[maior];
-   
-    for(int i = 0; i < maior; i++){
+    int vetAux[maior + 1];
+
+    for (int i = 0; i < maior + 1; i++) { 
         vetAux[i] = 0;
     }
-    
+
     for (int i = 0; i < tam; i++) {
         vetAux[vet[i]]++;
     }
-    
+
     int j = 0;
-    for (int i = 0; i < maior; i++) {
-        while(vetAux[i] != 0){
+    for (int i = 0; i < maior + 1; i++) { 
+        while (vetAux[i] != 0) {
             vet[j] = i;
             j++;
-            
+
             vetAux[i]--;
         }
     }
-    
-    exibir(vet, tam);
 }
